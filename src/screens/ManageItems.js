d6f849423e78e7c44213f480d9a896982b7234d7
Bhargav0528 <bhargavauden1998@gmail.com>
2020-03-11 17:13:08 +0530

    Save discard done

--- src/screens/ManageItems.js
+++ src/screens/ManageItems.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Image, ScrollView, Dimensions, TouchableOpacity, View, TouchableHighlight } from 'react-native';
+import { StyleSheet, Image, ScrollView, Dimensions, TouchableOpacity, View, TouchableHighlight, Alert, ToastAndroid } from 'react-native';
 import { Layout, Text, Icon } from 'react-native-ui-kitten';
 import {BoxShadow} from 'react-native-shadow';
 import { TouchableNativeFeedback } from 'react-native-gesture-handler';
@@ -10,6 +10,7 @@ import AntDesign from 'react-native-vector-icons/AntDesign'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import { Overlay, Input, Button } from 'react-native-elements';
+import Fontisto from 'react-native-vector-icons/Fontisto'
 
 const ViewportAwareView = Viewport.Aware(View)
 
@@ -28,35 +29,93 @@ export default class ManageItems extends React.Component{
     foodItemName:"",
     foodItemDescription: "",
     foodItemActualPrice: 0,
-  currentCategorySelected:null
+    currentCategorySelected:null,
+    isCategoryModalVisible: false,
+    categoryName: ""
   }
 
   async fetchItemsFromDB(restUUID) {
-    categoryList = []
-    items = {}
+    let categoryList = []
+    let items = {}
     await firebase.database().ref(`restaurants/${restUUID}/categories`).on('value',(categories=>{
       firebase.database().ref(`restaurants/${restUUID}/foodItems`).on('value',(foodItems=>{
+        categoryList = []
+        items={}
+      if(categories && categories.val() && categories!==null && categories.val()!==null ) {
+          console.log("DB",categories.val())
       Object.entries(categories.val()).map(([categoryId, categoryVal])=>{
         let categoryObject = {...categoryVal}
         categoryObject["categoryId"] = categoryId
         categoryList.push(categoryObject)
         items[categoryId] = []
       })
+    
+      if(foodItems!==null && foodItems.val() && foodItems.val()!==null && foodItems.val()) {
+      console.log("DB",foodItems.val())
       Object.entries(foodItems.val()).map(([foodItemId, foodItem])=>{
         let foodObject = {...foodItem}
         foodObject["quantity"] = 0
         foodObject["foodItemId"] = foodItemId
         foodObject["type"] = 0
+        if(items[foodItem.category])
         items[foodItem.category].push(foodObject)
       })
-      this.setState({categoryList:categoryList, currentCategorySelected:categoryList[0].categoryId, items:items})
-      }))
+    }
+  }
+      this.setState({categoryList:categoryList, currentCategorySelected:categoryList.length>0?categoryList[0].categoryId:null, items:items})
+    
+    }))
+    }))
+  }
+
+  async fetchDraft(restUUID) {
+    let categoryList = []
+    let items = {}
+    await firebase.database().ref(`restaurants/${restUUID}/draftFoodItems/categories`).on('value',(categories=>{
+      firebase.database().ref(`restaurants/${restUUID}/draftFoodItems/foodItems`).on('value',(foodItems=>{
+        categoryList = []
+        items={}
+      if(categories && categories.val() && categories!==null && categories.val()!==null ) {
+        console.log("Draft",categories.val())
+      Object.entries(categories.val()).map(([categoryId, categoryVal])=>{
+        let categoryObject = {...categoryVal}
+        categoryObject["categoryId"] = categoryId
+        categoryList.push(categoryObject)
+        items[categoryId] = []
+      })
+      if(foodItems!==null && foodItems.val() && foodItems.val()!==null && foodItems.val()) {
+        console.log("Draft",foodItems.val())
+      Object.entries(foodItems.val()).map(([foodItemId, foodItem])=>{
+        let foodObject = {...foodItem}
+        foodObject["quantity"] = 0
+        foodObject["foodItemId"] = foodItemId
+        foodObject["type"] = 0
+        if(items[foodItem.category])
+        items[foodItem.category].push(foodObject)
+      })
+    } 
+  }
+    if(categoryList.length!==0) {
+      this.setState({categoryList:categoryList, currentCategorySelected:categoryList.length>0?categoryList[0].categoryId:null, items:items})
+    } else {
+      console.log("No drft found")
+      ToastAndroid.showWithGravity(
+        'No draft found, loading user menu',
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER,
+      );
+      let restUUID = firebase.auth().currentUser.uid
+    this.fetchItemsFromDB(restUUID)
+    }
+    
+    }))
+    
     }))
   }
 
   componentDidMount() {
     let restUUID = firebase.auth().currentUser.uid
-    this.fetchItemsFromDB(restUUID)
+    this.fetchDraft(restUUID)
   }
 
   shiftCategory = (direction, categoryId, currentSelected=false) => {
@@ -104,17 +163,18 @@ export default class ManageItems extends React.Component{
       let title = this.state.foodItemName
       let description = this.state.foodItemDescription
       let actualPrice = this.state.foodItemActualPrice
+      let currentEpochTime = Date.now()
       if(title!=="" && actualPrice!=="") {
         if(/^\d+$/.test(actualPrice)) {
             let itemObject = {} 
-            itemObject["foodItemId"] = "food_"+Math.round((new Date()).getTime() / 1000);
+            itemObject["foodItemId"] = `food_${currentEpochTime}`;
             itemObject["category"] = this.state.currentCategorySelected
             itemObject["title"] = title
             itemObject["description"] = description
             itemObject["actualPrice"] = actualPrice
             itemObject["type"] = 0
             items[this.state.currentCategorySelected].push(itemObject)
-            this.setState({items:items, isVisible:false})
+            this.setState({items:items, isVisible:false, foodItemActualPrice:0, foodItemDescription:"",foodItemName:""})
         } else {
             alert("Please enter valid price")
         }
@@ -137,12 +197,30 @@ export default class ManageItems extends React.Component{
     this.setState({items:items})
   }
 
+  addCategory(categoryName) {
+    let currentEpochTime = Date.now()
+    let items = {...this.state.items}
+    let categoryList = [...this.state.categoryList]
+    let categoryId = `category_${currentEpochTime}`
+    categoryList.push({
+      categoryId: categoryId,
+      name: categoryName
+    })
+    items[categoryId] = []
+    this.setState({
+      items: items, 
+      isCategoryModalVisible:false,
+      categoryList: categoryList,
+      categoryName: ""
+    })
+  }
+
   renderItemList()
   {
-
     var items = []
     if(this.state.currentCategorySelected!==null) {
     var stateItems = this.state.items[this.state.currentCategorySelected]
+    if(stateItems!==undefined && stateItems!==null) {
     Object.entries(stateItems).forEach(([key,item]) => {
       items.push(<BoxShadow setting={{
         width: this.screenWidth *0.95,
@@ -197,44 +275,47 @@ export default class ManageItems extends React.Component{
   </View>
   </BoxShadow>)
     })
+
+    items.push(
+      <BoxShadow setting={{
+          width: this.screenWidth *0.95,
+          height: this.screenHeight * 0.15,
+          color: '#fff',
+          border: 5,
+          opacity: 0.2,
+          x: 0,
+          y: 4,
+          radius:this.screenHeight*0.02,
+          style:{
+            marginTop:this.screenHeight*0.01,
+            marginBottom: this.screenHeight*0.015,
+          }
+        }}>
+        <TouchableHighlight 
+        style={{
+            width:this.screenWidth *0.95, 
+            flexDirection:'row', 
+            height:this.screenHeight*0.15, 
+            backgroundColor:'#fff', 
+            borderRadius:this.screenHeight*0.02,
+            borderStyle:'dashed',
+            borderWidth:3,
+            borderColor:"#55C2FF80",
+            justifyContent:'center',
+            alignItems:'center',
+            backgroundColor:"#55C2FF30"}} 
+          onPress={()=>{this.setState({isVisible:true})}}>
+          <AntDesign
+              name="pluscircle" 
+              size={45} 
+              color="#55C2FF"
+          />
+        </TouchableHighlight>
+        </BoxShadow>
+    )
+    }
   }
-  items.push(
-    <BoxShadow setting={{
-        width: this.screenWidth *0.95,
-        height: this.screenHeight * 0.15,
-        color: '#fff',
-        border: 5,
-        opacity: 0.2,
-        x: 0,
-        y: 4,
-        radius:this.screenHeight*0.02,
-        style:{
-          marginTop:this.screenHeight*0.01,
-          marginBottom: this.screenHeight*0.015,
-        }
-      }}>
-      <TouchableHighlight 
-      style={{
-          width:this.screenWidth *0.95, 
-          flexDirection:'row', 
-          height:this.screenHeight*0.15, 
-          backgroundColor:'#fff', 
-          borderRadius:this.screenHeight*0.02,
-          borderStyle:'dashed',
-          borderWidth:3,
-          borderColor:"#55C2FF80",
-          justifyContent:'center',
-          alignItems:'center',
-          backgroundColor:"#55C2FF30"}} 
-        onPress={()=>{this.setState({isVisible:true})}}>
-        <AntDesign
-            name="pluscircle" 
-            size={45} 
-            color="#55C2FF"
-        />
-      </TouchableHighlight>
-      </BoxShadow>
-  )
+  
     return items
   }
 
@@ -257,8 +338,83 @@ export default class ManageItems extends React.Component{
   _handleViewportLeave = ()=>{
   }
 
+  deleteCategory() {
+    Alert.alert(
+      'Deleting category ...',
+      'Are you sure ?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => {
+          let currentCategorySelected = this.state.currentCategorySelected
+          let items = {...this.state.items}
+          delete items[currentCategorySelected]
+          let categoryList = [...this.state.categoryList]
+          let catIndex = null
+          let nextCat = null
+          categoryList.forEach((category, categoryIndex)=>{
+            if(category.categoryId === this.state.currentCategorySelected) {
+              catIndex = categoryIndex
+              if(categoryIndex==0 && categoryList.length>1)
+                nextCat = categoryList[categoryIndex+1].categoryId
+              else if(categoryIndex>0 && categoryList.length>1)
+                nextCat = categoryList[categoryIndex-1].categoryId
+            categoryList.splice(catIndex, 1)
+            this.setState({categoryList: categoryList, items:items, currentCategorySelected: nextCat!==null?  nextCat:null})
+          
+            }
+          })
+          
+        }},
+      ],
+      {cancelable: true},
+    );
+    
+  }
+
+  saveChanges() {
+    Alert.alert(
+      'Saving all changes ...',
+      'Are you sure ?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => {
+          let draftFoodItems = {}
+          let categories = {}
+          let foodItems = {}
+          this.state.categoryList.forEach(category=>{
+            categories[category.categoryId] = {name:category.name}
+          })
+          Object.entries(this.state.items).map(([category, item])=>{
+            item.forEach(foodItem=>{
+              foodItems[foodItem.foodItemId] = {
+                actualPrice: foodItem.actualPrice,
+                category: foodItem.category, 
+                title: foodItem.title,
+                description: foodItem.description,
+                type: foodItem.type,
+              }
+            })
+          })
+          draftFoodItems["categories"] = categories
+          draftFoodItems["foodItems"] = foodItems
+          firebase.database().ref(`restaurants/${firebase.auth().currentUser.uid}/draftFoodItems`).set(draftFoodItems)
+        }},
+      ],
+      {cancelable: true},
+    );
+  }
+
   render()
   {
+
   return(
     <View>
       <Viewport.Tracker>
@@ -301,8 +457,69 @@ export default class ManageItems extends React.Component{
         </BoxShadow>
     </View>
     </ViewportAwareView>
+    <View style={{
+      flexDirection:'row',
+      width:'100%',
+      justifyContent:'center',
+      alignItems:'center'
+    }}>
+    <View 
+      style={{
+        height:this.screenHeight*(0.1),
+        width: this.screenWidth*(0.3),
+        backgroundColor:'#fff',
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+        <Fontisto
+            name="undo" 
+            size={30} 
+            color="#ef5350"
+            onPress={()=>{
+              let restUUID = firebase.auth().currentUser.uid
+              this.fetchDraft(restUUID)
+            }}
+        />
+        <Text>Undo all changes</Text>
+    </View>
+    <View 
+      style={{
+        height:this.screenHeight*(0.1),
+        width: this.screenWidth*(0.3),
+        backgroundColor:'#fff',
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+        <Fontisto
+            name="undo" 
+            size={30} 
+            color="#ef5350"
+            onPress={()=>{
+                firebase.database().ref(`restaurants/${firebase.auth().currentUser.uid}/draftFoodItems`).remove()
+              
+            }}
+        />
+        <Text>Revert to user</Text>
+    </View>
+    <View 
+      style={{
+        height:this.screenHeight*(0.1),
+        width: this.screenWidth*(0.3),
+        backgroundColor:'#fff',
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+        <MaterialIcons
+            name="save" 
+            size={30} 
+            color="#4caf50"
+            onPress={()=>{this.saveChanges()}}
+        />
+        <Text>Save changes</Text>
+    </View>
+    </View>
     <View style={{width:'100%',height:this.screenHeight*0.1,top:this.screenHeight*0.035}}>
-    <ScrollView style={{flex:1, }} horizontal={true} showsHorizontalScrollIndicator={false}>
+    <ScrollView style={{flex:1}} horizontal={true} showsHorizontalScrollIndicator={false}>
     {
       this.state.categoryList.map(category=>{
         if(this.state.currentCategorySelected==category.categoryId)
@@ -354,11 +571,56 @@ export default class ManageItems extends React.Component{
       })
     
     }
+    {
+      <AntDesign
+        name="pluscircle"
+        size={30}
+        color="#55C2FF"
+        style={{width:this.screenWidth*(0.12)}}
+        onPress={()=>{this.setState({isCategoryModalVisible:true})}}
+      />
+    }
     </ScrollView>
     </View>
 
     <Layout style={{paddingLeft:'2%'}}>
-
+      {
+        this.state.currentCategorySelected!==null?
+        <BoxShadow setting={{
+          width: this.screenWidth *0.95,
+          height: this.screenHeight * 0.07,
+          color: '#000',
+          border: 5,
+          opacity: 0.2,
+          x: 0,
+          y: 4,
+          radius:this.screenHeight*0.02,
+          style:{
+            marginTop:this.screenHeight*0.01,
+            marginBottom: this.screenHeight*0.015
+          }
+        }}>
+        <TouchableOpacity 
+          style={{
+            backgroundColor:'#fff',
+            height:'100%',
+            width:'100%',
+            borderRadius:this.screenHeight*0.02,
+            flexDirection:'row',
+            alignItems:'center',
+            justifyContent:'center'}} 
+            onPress={()=>{this.deleteCategory()}}>
+              <MaterialIcons
+            name="delete" 
+            size={30} 
+            color="#ef5350"
+            
+        />
+          <Text>Delete Category</Text>
+        </TouchableOpacity>
+        </BoxShadow>
+        :null
+      }
       {this.renderItemList()}
 
     </Layout>
@@ -412,6 +674,45 @@ label="Price"
   rasied={true}
   buttonStyle={{backgroundColor:'#ef5350'}}
   onPress={()=>this.setState({ isVisible: false })}
+/>
+</View>
+
+  </View>
+</Overlay>
+
+
+<Overlay
+  isVisible={this.state.isCategoryModalVisible}
+  onBackdropPress={() => this.setState({ isCategoryModalVisible: false })}
+  borderRadius={20}
+  height="40%"
+  containerStyle={{marginTop:'8%'}}
+>
+    <View style={{width:'100%', height:'100%', justifyContent:'center', alignItems:'center'}}>
+  <Text style={{fontSize:18, fontWeight:'bold',marginBottom:'6%'}}>Enter Category Details</Text>
+  <Input
+label="Category"
+  placeholder='Enter category name'
+  onChangeText={text => this.setState({categoryName:text})}
+  value={this.state.categoryName}
+    />
+<View style={{
+  flexDirection:'row', 
+  marginTop:'7%', 
+  width:'50%', 
+  justifyContent:'space-between', 
+  alignItems:'center'}}>
+<Button
+  title="Add"
+  rasied={true}
+  buttonStyle={{backgroundColor:'#4caf50'}}
+  onPress={this.addCategory.bind(this, this.state.categoryName)}
+/>
+<Button
+  title="Cancel"
+  rasied={true}
+  buttonStyle={{backgroundColor:'#ef5350'}}
+  onPress={()=>this.setState({ isCategoryModalVisible: false })}
 />
 </View>
 
